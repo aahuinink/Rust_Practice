ede6b95d7f4dc8cdae5ee884bd16e619d6b0ea8f
aahuinink <a_huinink@hotmail.ca>
2024-07-09 14:56:22 -0700

    refactored Config struct into GrepObject struct that uses iterators. Hid "search" and wrapped in "run" function

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -2,91 +2,58 @@ use std::error::Error;
 use std::fs;
 use std::env;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents: String = fs::read_to_string(config.file_path)?;
-    let result = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
-    } else {
-        search_case_sensitive(&config.query, &contents)
-    };
-
-    for line in result {
-        println!("{line}");
-    }
-    Ok(())
-}
-
-pub struct Config 
+pub struct GrepObject
 {
     pub query: String,
     pub file_path: String,
     pub ignore_case: bool
 }
 
-impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+impl GrepObject {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<GrepObject, &'static str> {
+        args.next();
 
-        if args.len() == 3 {
-            let query = args[1].clone();
-            let file_path = args[2].clone();
-            let ignore_case: bool = env::var("IGNORE_CASE").is_ok();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
 
-            return Ok(Config { query, file_path, ignore_case})
+        let file_path: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path")
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(GrepObject { query, file_path, ignore_case })
+    }
+
+    pub fn run(&self) -> Result<(), Box<dyn Error>> {
+        let contents: String = if self.ignore_case {
+            fs::read_to_string(&self.file_path)?.to_lowercase()
+        } else {
+            fs::read_to_string(&self.file_path)?
+        };
+
+        let query = if self.ignore_case {
+            &self.query.to_lowercase()
+        } else {
+            &self.query
+        };
+
+        let result = search(&query, &contents);
+
+        for line in result.iter() {
+            println!("{line}");
         }
-        
-        Err("Please provide a search term and a file path")
+
+        Ok(())
     }
 }
 
-pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
+    fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+        contents
+            .lines()
+            .filter(|line| line.contains(query))
+            .collect()
     }
-    results
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&str> = Vec::new();
-    let query = query.to_lowercase();
-    
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query)
-        {
-            results.push(line);
-        }
-    }
-    results
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn case_sensitive() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape";
-        
-        assert_eq!(vec!["safe, fast, productive."], search_case_sensitive(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query: &str = "rUsT";
-        let contents: &str = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-        
-        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
-    }
-
-}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,15 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+use minigrep::GrepObject;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+    let grep_object: GrepObject = GrepObject::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = grep_object.run() {
         eprintln!("Application error: {e}");
         process::exit(1);
     }

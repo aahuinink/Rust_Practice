52a51e7ef0667522734ac03b623e21f844ec3f5b
aahuinink <a_huinink@hotmail.ca>
2024-07-11 15:12:30 -0700

    done

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,80 +1,92 @@
 use std::{
-    fs, io::{prelude::*, BufReader}, net::TcpStream, sync::{mpsc::{self, Receiver}, Arc, Mutex}, thread::{self, JoinHandle}
+    sync::{mpsc, Arc, Mutex},
+    thread,
 };
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
-struct Worker {
-    id: usize,
-    thread: JoinHandle<Arc<Mutex<Receiver<Job>>>>,
-}
-
-impl Worker {
-    /// Creates a new worker struct that can wait for jobs sent to the thread pool
-    /// 
-    /// 
-    fn new(id: usize, reciever: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread: JoinHandle<Arc<Mutex<Receiver<Box<dyn FnOnce() + Send>>>>> = thread::spawn(move || loop {
-            let job: Box<dyn FnOnce() + Send> = reciever.lock().unwrap().recv().unwrap();
-            println!("Worker {id} got a job; executing.");
-
-            job();
-        });
-        Worker { id, thread}
-    }
-}
-
-pub struct ThreadPool {
-    sender: mpsc::Sender<Job>,
-    workers: Vec<Worker>,
-}
-
 impl ThreadPool {
     /// Create a new ThreadPool.
-    /// 
+    ///
     /// The size is the number of threads in the pool.
-    /// 
-    ///  # Panics
-    /// 
-    /// The 'new' function panics if the size is 0.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        let mut workers: Vec<Worker> = Vec::with_capacity(4);
 
-        let (sender, reciever) = mpsc::channel();
+        let (sender, receiver) = mpsc::channel();
 
-        let reciever = Arc::new(Mutex::new(reciever));
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&reciever)));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { sender, workers }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
-        where
-            F: FnOnce() + Send + 'static,
-        {
-            let job = Box::new(f);
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
 
-            self.sender.send(job).unwrap();
-        }
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
 
-pub fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP /1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP /1.1 404 NOT FOUND", "404.html")
-    };
-    let contents: String = fs::read_to_string(filename).unwrap();
-    let length: usize = contents.len();
-    let response: String = 
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
+
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
 }
